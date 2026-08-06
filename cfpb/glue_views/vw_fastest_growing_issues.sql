e5de4a0a05a9bcc1064ddc4ea90c8bb50fab42a7
Ryan Lin <Ryanlin0861@gmail.com>
2026-08-06 19:37:54 -0400

    query fix
    return 0 when there is entries for this month of issue x sub issue

--- cfpb/glue_views/vw_fastest_growing_issues.sql
+++ cfpb/glue_views/vw_fastest_growing_issues.sql
@@ -1,6 +1,27 @@
 CREATE VIEW "cfpb-complaints".vw_fastest_growing_issues AS
 WITH
-  m AS (
+  months AS (
+   SELECT DISTINCT month_received
+   FROM
+     vw_cfpb_base
+)
+, pairs AS (
+   SELECT DISTINCT
+     issue
+   , sub_issue
+   FROM
+     vw_cfpb_base
+)
+, grid AS (
+   SELECT
+     months.month_received
+   , pairs.issue
+   , pairs.sub_issue
+   FROM
+     months
+   CROSS JOIN pairs
+)
+, m AS (
    SELECT
      month_received
    , issue
@@ -9,25 +30,37 @@ WITH
    FROM
      vw_cfpb_base
    GROUP BY month_received, issue, sub_issue
-) 
+)
+, filled AS (
+   SELECT
+     grid.month_received
+   , grid.issue
+   , grid.sub_issue
+   , COALESCE(m.complaints, 0) complaints
+   FROM
+     grid
+   LEFT JOIN m ON (grid.month_received = m.month_received)
+      AND (grid.issue = m.issue)
+      AND (grid.sub_issue = m.sub_issue)
+)
 , g AS (
    SELECT
      month_received
    , issue
    , sub_issue
    , complaints
-   , LAG(complaints) OVER (PARTITION BY issue, sub_issue ORDER BY month_received ASC) prev_complaints
+   , COALESCE(LAG(complaints) OVER (PARTITION BY issue, sub_issue ORDER BY month_received ASC), 0) prev_complaints
    FROM
-     m
-) 
+     filled
+)
 SELECT
   month_received
 , issue
 , sub_issue
 , complaints
 , prev_complaints
-, ROUND(((1E2 * (complaints - prev_complaints)) / prev_complaints), 1) mom_growth_pct
-, RANK() OVER (PARTITION BY month_received ORDER BY (((complaints - prev_complaints) * 1E0) / prev_complaints) DESC) growth_rank
+, ROUND(((1E2 * (complaints - prev_complaints)) / NULLIF(prev_complaints, 0)), 1) mom_growth_pct
+, RANK() OVER (PARTITION BY month_received ORDER BY (((complaints - prev_complaints) * 1E0) / NULLIF(prev_complaints, 0)) DESC) growth_rank
 FROM
   g
 WHERE (prev_complaints >= 10)
